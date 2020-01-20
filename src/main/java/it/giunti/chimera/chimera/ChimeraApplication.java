46dca4cf51f86303c1790605e3dcef85cdc66c3f
Paolo Tacconi <p.tacconi@giunti.it>
2020-01-20 15:40:44 +0100

    base project

---
+++ src/main/java/it/giunti/chimera/chimera/ChimeraApplication.java
@@ -0,0 +1,13 @@
+package it.giunti.chimera.chimera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChimeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/giunti/chimera/chimera/ChimeraApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giunti.chimera.chimera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
